1de98d1101231324aaf84e7ce730087aeb43839e
DanielReis <danielr@kyros.com.br>
2019-11-01 15:57:48 -0300

    CURSO THYMELEAF

---
+++ src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -0,0 +1,125 @@
+package com.mballem.curso.boot.web.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mballem.curso.boot.domain.Departamento;
+import com.mballem.curso.boot.service.DepartamentoService;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+@Controller
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
+
+	@GetMapping("/cadastrar")
+	public String cadastrar(Departamento departamento) {
+		return "departamento/cadastro";
+	}
+	
+	@GetMapping("/listar")
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
+		return "departamento/lista"; 
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(@Valid Departamento departamento, BindingResult result, RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "departamento/cadastro";
+		}
+		
+		service.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "departamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(@Valid Departamento departamento, BindingResult result, RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "departamento/cadastro";
+		}
+		
+		service.editar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		
+		if (service.departamentoTemCargos(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargo(s) vinculado(s).");
+		} else {
+			service.excluir(id);
+			model.addAttribute("success", "Departamento excluído com sucesso.");
+		}
+		
+		return listar(model);
+	}
+
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@PostMapping("/imprimir")	
+	public void imprimir(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException {
+		
+		parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+		
+		// Pega o arquivo .jasper localizado em resources
+		InputStream jasperStream = this.getClass().getResourceAsStream("/relatorios/livros.jasper");
+		
+		// Cria o objeto JaperReport com o Stream do arquivo jasper
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, no caso uma conexão ao banco de dados
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
+
+		// Configura a respota para o tipo PDF
+		response.setContentType("DemoMvcApplication/pdf");
+		// Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
+		// para fazer download do relatório troque 'inline' por 'attachment'
+		response.setHeader("Content-Disposition", "inline; filename=livros.pdf");
+
+		// Faz a exportação do relatório para o HttpServletResponse
+		final OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+	}	
+	
+	
+}
